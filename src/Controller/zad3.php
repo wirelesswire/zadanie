485c2f00e73cc1cb90d95133e71d7714c49f0a96
wire <brnrml007@gmail.com>
2023-08-14 16:36:56 +0200

    szukanie działa

--- src/Controller/zad3.php
+++ src/Controller/zad3.php
@@ -14,7 +14,7 @@ class zad3 extends AbstractController
         $encoders = [ new JsonEncoder()];
         $normalizers = [new ObjectNormalizer(),new ArrayDenormalizer()];
         $serializer = new Serializer($normalizers, $encoders);
-        $json =     file_get_contents("https://gorest.co.in/public/v2/users");
+        $json =     file_get_contents("https://gorest.co.in/public/v2/users?page=1&per_page=25 ");
         $users = $serializer->deserialize($json,"App\Entity\user[]","json");
         return $users;
     }
@@ -32,8 +32,31 @@ class zad3 extends AbstractController
     #[Route('/zad3/render', name: 'render')]
     public function index(): Response
     {
-        return $this->render("tabelka.html.twig",['users' => $this->getUsers(),]);
+        $stringToMatch = $_POST['str'] ??null;
+        // var_dump(($stringToMatch));
+        if($stringToMatch   == null  || $stringToMatch == "" || $stringToMatch == "undefined"){
+        return $this->render("tabelka.html.twig",['users' => $this->getUsers() ,]);
+
+        }
+        return $this->render("tabelka.html.twig",['users' =>$this->szukaj( $this->getUsers() ,$stringToMatch ) ,]);
+
     }
+    public function szukaj($users , $stringToMatch ){
+        $usersthatmatch = [];
+
+        foreach($users as $user ){
+            if(str_contains( $user->getId().$user->getName().$user->getEmail().$user->getGender().$user->getStatus()  ,$stringToMatch  )){
+                $usersthatmatch[] = $user;
+            }
+        }
+
+        
+
+
+
+        return $usersthatmatch;
+    }
+
 
 
 
